4de3c2d5967c30786003c25df10ed8cf033590e9
Mostafa Nema Rasheed <mostafa.n3ma@gmail.com>
2021-11-24 06:52:06 +0300

    problem fixed attempt2

--- app/src/main/java/com/udacity/shoestore/models/ShoeViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/models/ShoeViewModel.kt
@@ -25,11 +25,7 @@ class ShoeViewModel : ViewModel() {
     }
 
     fun initShoeList() {
-        var shoeList:MutableList<Shoe>
-        val shoe1:Shoe= Shoe("classic snickers",42.0,"Nike","nice shoe", listOf("1","2") )
-        val shoe2:Shoe= Shoe("Golf shoes",38.0,"puma","nice shoe2",listOf("3","4") )
-        val shoe3:Shoe= Shoe("Running shoes",29.0,"adidas","nice shoe3",listOf("3","4") )
-        shoeList= mutableListOf(shoe1,shoe2,shoe3)
+        var shoeList:MutableList<Shoe> = mutableListOf()
         _shoeList.value=shoeList
 
     }
@@ -37,10 +33,7 @@ class ShoeViewModel : ViewModel() {
         val shoe:Shoe= Shoe(shoeName.value.toString(),
             shoeSize.value!!.toDouble(),company.value.toString(),description.value.toString(),
             listOf("1","2"))
-        var shoeList:MutableList<Shoe>
-        shoeList=_shoeList.value as MutableList<Shoe>
-        shoeList.add(shoe)
-        _shoeList.value=shoeList
+      _shoeList.value!!.add(shoe)
         onDetailsFinished()
     }
 
